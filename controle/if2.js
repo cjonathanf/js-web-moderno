63dc17b15324d31bb7e7c03a0ab44e5af26b35ac
cjonathanf <jooh.cferreira@gmail.com>
2020-11-11 00:06:53 -0300

    aulas_novas

---
+++ controle/if2.js
@@ -0,0 +1,18 @@
+function teste1(numero) {
+    if (numero > 7)
+        console.log(numero)
+    console.log("Final")
+
+}
+
+teste1(6)
+teste1(8)
+
+function teste2(numero) {
+    if (numero > 7) {
+        console.log(numero)
+    }
+}
+
+teste2(6)
+teste2(8)
