2ba8284a7b0be109ee24bf17cf41f3b4c55a11c7
cjonathanf <jooh.cferreira@gmail.com>
2020-11-12 00:32:01 -0300

    exercicios

---
+++ exercicios/exercicioIdade.js
@@ -0,0 +1,9 @@
+const converterIdadesEmAnosParaDias = function (idade) {
+    paraDias = idade * 365
+    console.log(`Seus anos em dias e um total de: ${paraDias}`)
+}
+
+converterIdadesEmAnosParaDias(28)
+
+
+
